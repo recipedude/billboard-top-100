586341908f6359df495bd5867a4f002f4a156195
Sean Wenzel <sean@wenzel.net>
2018-11-09 15:26:39 -0500

    Update test to use ES6 syntax and pass eslint check
    Add fail condition when Rank 1 is not present

--- tests/test.js
+++ tests/test.js
@@ -1,11 +1,33 @@
-var billboard = require("../billboard-top-100.js").getChart;
-var listCharts = require("../billboard-top-100.js").listCharts;
+var billboard = require('../billboard-top-100.js').getChart
+var listCharts = require('../billboard-top-100.js').listCharts
 
-listCharts(function(err, data){
-    console.log(data);
-});
+listCharts(function (err, data) {
+  console.log('=== listCharts - start ===')
+  if (err) {
+    console.log('ERROR!')
+    console.log(err)
+  } else {
+    console.log(JSON.stringify(data, null, 2))
+  }
+  console.log('--- listCharts - finish ---\n\n')
+})
 
-billboard('hot-100', '2016-11-19', function(err, songs){
-    if (err) console.log(err);
-    console.log(songs);
-});
+billboard('hot-100', '2016-11-19', function (err, songs) {
+  console.log('=== hot-100 test - start ===')
+  if (err) {
+    console.log('ERROR!')
+    console.log(err)
+  } else {
+
+    // check if first rank is present
+    if (songs[0].rank !== '1') {
+      console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+      console.log('FAIL!!! - Rank 1 is missing!')
+      console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+    }
+
+    console.log(songs)
+
+  }
+  console.log('--- hot-100 - finish ---')
+})
